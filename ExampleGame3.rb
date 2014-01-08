38052d25b5445b18173b253d268bda2be81d616e
fnc314 <fnc314@gmail.com>
2014-01-08 01:42:03 -0800

    code-upkeep_indentation

--- ExampleGame3.rb
+++ ExampleGame3.rb
@@ -1,63 +1,61 @@
 i=1
-
 key = false
 
 while !key
+	pages_with_questions = {}
 
-pages_with_questions = {}
+	lines_grabbed = ""
+	key = ""
 
-lines_grabbed = ""
-key = ""
-
-File.open("story.txt", "r") do |infile|
-	while (line = infile.gets)
-		if line.index("~p")
-			key = line.chomp
-			lines_grabbed = ""
-		else
-			lines_grabbed += line
+	File.open("story.txt", "r") do |infile|
+		while (line = infile.gets)
+			if line.index("~p")
+				key = line.chomp
+				lines_grabbed = ""
+			else
+				lines_grabbed += line
+			end
+			pages_with_questions[key] = lines_grabbed
 		end
-		pages_with_questions[key] = lines_grabbed
 	end
-end
 
-puts
-puts "Let's play a game."
-puts "Pretend you just woke up..."
-puts 
-puts "Goodmorning"
-puts pages_with_questions["~p1"]
+	puts
+	puts "Let's play a game."
+	puts "Pretend you just woke up..."
+	puts 
+	puts "Goodmorning"
+	puts pages_with_questions["~p1"]
 
-puts "1)  #{pages_with_questions["~p1:c1:p2"]}"
-puts "2)  #{pages_with_questions["~p1:c2:p3"]}"
-puts "3)  #{pages_with_questions["~p1:c3:p4"]}"
-puts "Select 1, 2, or 3"
+	puts "1)  #{pages_with_questions["~p1:c1:p2"]}"
+	puts "2)  #{pages_with_questions["~p1:c2:p3"]}"
+	puts "3)  #{pages_with_questions["~p1:c3:p4"]}"
+	puts "Select 1, 2, or 3"
 
-mode = false
-while !mode
-	c = gets.chomp.to_i + 1
-	if [2, 3, 4].include?(c)
-		puts pages_with_questions["~p#{c}"]
-		mode = true
-	else
-		puts "Try again...select 1, 2, or 3 ONLY!"
+	mode = false
+	while !mode
+		c = gets.chomp.to_i + 1
+		if [2, 3, 4].include?(c)
+			puts pages_with_questions["~p#{c}"]
+			mode = true
+		else
+			puts "Try again...select 1, 2, or 3 ONLY!"
+		end
 	end
-end
 
-puts "Would you like to play again? (y/n)"
-answer = gets.chomp.to_s
+	puts "Would you like to play again? (y/n)"
+	answer = gets.chomp.to_s
 
-if answer == "y" || answer == "Y"
-	key = false
-	i += 1
-elsif answer == "n" || answer == "N"
-	key = true
-	if i == 1
-		puts "You played once!  Thanks for playing."
-	else 
-		puts "You played #{i} times!  Thanks for playing."
+	if answer == "y" || answer == "Y"
+		key = false
+		i += 1
+	elsif answer == "n" || answer == "N"
+		key = true
+		if i == 1
+			puts "You played once!  Thanks for playing."
+		else 
+			puts "You played #{i} times!  Thanks for playing."
+		end
 	end
-end
 
 end
 
@@ -66,7 +64,7 @@ mode = false
 while !mode
 	choice = gets.chomp.to_i
 	if choice == 1
-		puts pages_with_questions["~p1"]
+		puts pages_with_questions["~p2"]
 		mode = true
 	elsif choice == 2
 		puts pages_with_questions["~p3"]
